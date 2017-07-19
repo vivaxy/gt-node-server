cb42b9768eeddbe7b48ff0e42f1a7a1dc0bca38f
vivaxy <xyxuye2007@126.com>
2017-07-19 14:52:19 +0800

    feat(log): :sparkles:Update log middleware

--- middlewares/log.js
+++ middlewares/log.js
@@ -9,7 +9,16 @@ const getTimeStamp = () => {
     return new Date().getTime();
 };
 
-module.exports = async(ctx, next) => {
+const stringify = (json) => {
+    return JSON.stringify(json, (k, v) => {
+        if (Array.isArray(v)) {
+            return [v.length];
+        }
+        return v;
+    });
+};
+
+module.exports = async (ctx, next) => {
     const request = ctx.request;
 
     const startTime = getTimeStamp();
@@ -19,5 +28,5 @@ module.exports = async(ctx, next) => {
     } catch (ex) {
         logger.error(ex);
     }
-    logger.info(`[request] ${request.method} ${request.path} in ${getTimeStamp() - startTime}ms`);
+    logger.info(`[request] ${request.method} ${request.path}; params=${stringify(ctx.params)}, query=${stringify(ctx.query)}, body=${stringify(ctx.request.body)}; [response] status=${ctx.status}, body=${stringify(ctx.body)}; [time] ${getTimeStamp() - startTime}ms`);
 };
